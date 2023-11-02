3f59fe74fe303c6d0cc249679784134e45036744
MarcuX009 <pk421933605@gmail.com>
2023-11-01 22:47:42 -0700

    init ChatPage and ChatScrren

--- src/screens/stack/chatScreen/ChatScreen.tsx
+++ src/screens/stack/chatScreen/ChatScreen.tsx
@@ -1,27 +1,66 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {AppStackPageProps} from '../../../navigation/navigators/StackNavigator';
-import {signInWithGoogle} from '../../../../auth/useAuth';
+import { View, Text, StyleSheet, Pressable, PixelRatio, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import { AppStackPageProps } from '../../../navigation/navigators/StackNavigator';
+import { signInWithGoogle } from '../../../../auth/useAuth';
+
+import { styles } from './ChatScreenStyles';
+
+const DEVICEPIXELRATIO = PixelRatio.get();
 
 export type ChatScreenProps = {
   id: string;
+  chatName: string;
 };
 
 export default function ChatScreen({
   route,
   navigation,
 }: AppStackPageProps<'chat'>) {
+  const { chatName } = route.params;
   return (
-    <View
-      style={{flex: 1, justifyContent: 'space-evenly', alignItems: 'center'}}>
-      <Text
-        style={{
-          color: '#000',
-          fontWeight: 'bold',
-          fontSize: 20,
-        }}>
-        chat
-      </Text>
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}>
+          <Text style={styles.backButton}>Back</Text>
+        </TouchableOpacity>
+        {/* chat name dont show here, need to fix later*/}
+        <Text style={styles.headerText}>{chatName}</Text>  
+      </View>
+
+      {/* Message List */}
+      <ScrollView style={styles.messageList}>
+        <Text style={styles.message}>Example message1</Text>
+        <Text style={styles.message}>Example message2</Text>
+        <Text style={styles.message}>Example message3</Text>
+        <Text style={styles.message}>Example message4</Text>
+        <Text style={styles.message}>Example message5</Text>
+        <Text style={styles.message}>Example message6</Text>
+        <Text style={styles.message}>Example message7</Text>
+        <Text style={styles.message}>Example message8</Text>
+        <Text style={styles.message}>Example message9</Text>
+        <Text style={styles.message}>Example message10</Text>
+        <Text style={styles.message}>Example message11</Text>
+        <Text style={styles.message}>Example message12</Text>
+        <Text style={styles.message}>Example message13</Text>
+        <Text style={styles.message}>Example message14</Text>
+        <Text style={styles.message}>Example message15</Text>
+        <Text style={styles.message}>Example message16</Text>
+        <Text style={styles.message}>Example message17</Text>
+        <Text style={styles.message}>Example message18</Text>
+        <Text style={styles.message}>Example message19</Text>
+        <Text style={styles.message}>Example message20</Text>
+      </ScrollView>
+
+      {/* Input Area */}
+      <View style={styles.inputArea}>
+        <TextInput style={styles.input} placeholder="Type a message" />
+        <TouchableOpacity>
+          <Text style={styles.sendButton}>Send</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
--- src/screens/tab/chat/ChatPage.tsx
+++ src/screens/tab/chat/ChatPage.tsx
@@ -1,69 +1,61 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, Pressable, PixelRatio, TouchableOpacity, ScrollView } from 'react-native';
-//import {TabPageProps} from '../../navigation/navigators/TabNavigator';
-import {signOut} from '../../../../auth/useAuth';
-import {useSelector} from 'react-redux';
-import {TabPageProps} from '../../../navigation/navigators/TabNavigator';
+import { signOut } from '../../../../auth/useAuth';
+import { useSelector } from 'react-redux';
+import { TabPageProps } from '../../../navigation/navigators/TabNavigator';
 
-
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { appStackNavigate } from '../../../navigation/navigators/StackNavigator';
-
-import { styles } from '../styles/ChatPageStyles';
-
-
+import { styles } from './ChatPageStyles';
+import ChatScreen from '../../stack/chatScreen/ChatScreen';
 
 export type ChatPageProps = EmptyProps;
 
-export default function ChatPage({route, navigation}: TabPageProps<'chats'>) {
+export default function ChatPage({ route, navigation }: TabPageProps<'chats'> & { navigation: any }) {
   return (
     <View style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
-        <Text style={styles.headerText}>CHAT</Text>
+        <Text style={styles.headerText}>CHAT LIST</Text>
       </View>
-      {/* <Pressable
-        style={styles.backButton}
-        onPress={() => navigation.navigate('About')}
+
+      {/* Back Button */}
+      <Pressable
+        style={styles.joinButton}
       >
-        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 14 }}>Back</Text>
-      </Pressable> */}
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 14 }}>To Join</Text>
+      </Pressable>
+
+      {/* Current Class List*/}
       <Text style={styles.category}>Current Class</Text>
+
       {/* Chat List */}
       <View style={styles.chatList}>
-        {/* Repeat this block for each chat item */}
-        <TouchableOpacity style={styles.chatItem} 
-        onPress={() => navigation.navigate('ChatRoom', { chatName: 'Class 1' })} 
+        <TouchableOpacity style={styles.chatItem}
+        // onPress={() => navigation.navigate(ChatScreen, { chatName: 'Class 1' })}
+        onPress={() => navigation.navigate('chat', { screen: 'ChatScreen', params: { chatName: 'Class 1' } })}
         >
           <Text style={styles.chatName}>Class 1</Text>
           <Text style={styles.chatMessage}>Sender: This is a preview of the last sent message...</Text>
         </TouchableOpacity>
-        {/* ... */}
       </View>
       <View style={styles.chatList}>
-        {/* Repeat this block for each chat item */}
-        <TouchableOpacity style={styles.chatItem}>
+        <TouchableOpacity style={styles.chatItem}
+        onPress={() => navigation.navigate('chat', { screen: 'ChatScreen', params: { chatName: 'Class 2' } })}>
           <Text style={styles.chatName}>Class 2</Text>
           <Text style={styles.chatMessage}>Sender: This is a preview of the last sent message...</Text>
         </TouchableOpacity>
-        {/* ... */}
       </View>
+
+      {/* Waitlist Class List */}
       <Text style={styles.category}>Waitlist Class</Text>
+
       <View style={styles.chatList}>
-        {/* Repeat this block for each chat item */}
-        <TouchableOpacity style={styles.chatItem}>
+        <TouchableOpacity style={styles.chatItem}
+        onPress={() => navigation.navigate('chat', { screen: 'ChatScreen', params: { chatName: 'Class 3' } })}>
           <Text style={styles.chatName}>Class 3</Text>
           <Text style={styles.chatMessage}>Sender: This is a preview of the last sent message...</Text>
         </TouchableOpacity>
-        {/* ... */}
       </View>
-      <Pressable
-        style={styles.joinButton}
-        // onPress={() => navigation.navigate('About')}
-      >
-        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 14 }}>To Join</Text>
-      </Pressable>
+      
     </View>
   );
 }
