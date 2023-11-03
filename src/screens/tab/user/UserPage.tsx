caea6588bce67b717a4be45e9d6c4751eaacfdc4
binyuzhong <91632366+binyuzhong@users.noreply.github.com>
2023-11-02 19:30:03 -0700

    Binyu zhong hw3 (#56)
    * binyuZhongHW3_and_userPage
    
    * change_with_useAuth.ts

--- src/screens/tab/user/UserPage.tsx
+++ src/screens/tab/user/UserPage.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import {View, Text, Button, Pressable} from 'react-native';
+
+import React, { useState } from "react";
+import {View,
+        Text,
+        Button,
+        Pressable,
+        SafeAreaView,
+        Image,
+        StatusBar,
+        TouchableOpacity,
+        Linking } from 'react-native';
 //import {TabPageProps} from '../../navigation/navigators/TabNavigator';
 import {signOut} from '../../../../auth/useAuth';
+import { Icon } from 'react-native-elements';
+import {styles} from '../user/UserPageStyle';
 import {useSelector} from 'react-redux';
 import {TabPageProps} from '../../../navigation/navigators/TabNavigator';
 
@@ -12,36 +23,80 @@ export default function UserPage({route, navigation}: TabPageProps<'user'>) {
   const user = useSelector(
     (state: ReduxState) => state.data.usermap[myUserId!],
   );
-
+    const [boxHeight, setBoxHeight] = useState(0);
+    const [isMuted, setIsMuted] = useState(false);
+    const [isActive, setIsActive] = useState(false);
   return (
-    <View
-      style={{flex: 1, justifyContent: 'space-evenly', alignItems: 'center'}}>
-      <Text
-        style={{
-          // color: '#fff',
-          fontWeight: 'bold',
-          fontSize: 20,
-        }}>
-        {'User Page'}
-      </Text>
-      <Pressable onPress={signOut}>
-        <Text
-          style={{
-            // color: '#fff',
-            fontWeight: 'bold',
-            fontSize: 30,
-          }}>
-          Sign Out
-        </Text>
-      </Pressable>
+    <View style={styles.container}>
+    <StatusBar backgroundColor="white" barStyle="dark-content" />
+    <SafeAreaView>
+        <View style={styles.headerWrapper}>
+            <Image 
+                source={require("../../../utilities/image/userImage.png")}
+                style={styles.profileImage} 
+            />
+            <View style={styles.userInfoContainer}>
+                <Text style={styles.userName}>{user?.firstName}</Text>
+                <Text style={styles.userEmail}>{user?.email}</Text>
+            </View>
+        </View>
+        <View style={styles.userLinksContainer}
+            onLayout={(event) => {
+                const containerWidth = event.nativeEvent.layout.width;
+                setBoxHeight(containerWidth * 0.35);
+            }}
+        >
+            <TouchableOpacity
+                style={[styles.box, { height: boxHeight }]}
+                activeOpacity={0.6}
+                onPress={() => Linking.openURL('https://www.canvas.ucsb.edu/')}
+            >
+                <Text style={styles.userLinks}>Canvas</Text>
+                <Icon name="link-outline" type='ionicon' color="black"/>
+            </TouchableOpacity>
 
-      <Text
-        style={{
-          // color: '#fff',
-          fontSize: 20,
-        }}>
-        {`${user?.email}\n${user?.firstName}\n${user?.lastName}`}
-      </Text>
-    </View>
+            <TouchableOpacity
+                style={[styles.box, { height: boxHeight }]}
+                activeOpacity={0.6}
+                onPress={() => Linking.openURL('https://my.sa.ucsb.edu/gold/')}
+            >
+                <Text style={styles.userLinks}>Gold</Text>
+                <Icon name="link-outline" type='ionicon' color="black"/>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.settingBarContainer}>
+            <TouchableOpacity 
+                style={[styles.longBox, isActive ? {backgroundColor: '#F883793D'} : {}]}
+                activeOpacity={0.6} 
+                onPressIn={() => setIsActive(true)}
+                onPressOut={() => setIsActive(false)}
+                onPress={() => setIsMuted(!isMuted)}
+            >
+                <Text style={styles.longBarText}>Status</Text>
+                <Icon 
+                    style={styles.longBoxIcon} 
+                    name={isMuted ? "book-outline" : "bed-outline"} 
+                    type='ionicon' 
+                    color="black"
+                />
+            </TouchableOpacity>
+            <View style={styles.longBox}>
+                <Text style={styles.longBarText}>Settings</Text>
+                <Icon style={styles.longBoxIcon} name="settings-outline" type='ionicon' color="black"/>
+            </View>
+            <TouchableOpacity
+                style={[styles.longBox, {borderBottomWidth: 0}]}
+                activeOpacity={0.6}
+                onPress={() => signOut()}
+            >
+                <Text style={styles.longBarText}>Sign Out</Text>
+                <Icon style={styles.longBoxIcon} name="log-out-outline" type='ionicon' color="black"/>
+            </TouchableOpacity>
+
+            
+        </View>
+    </SafeAreaView>
+</View>
+
   );
 }
