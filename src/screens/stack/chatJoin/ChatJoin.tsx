ecc0afa983a76da8935b552319f8a0ae84f57bb6
Jinghan Zhang <jessizhang03@gmail.com>
2023-11-15 01:52:21 -0800

    add search function

--- src/screens/stack/chatJoin/ChatJoin.tsx
+++ src/screens/stack/chatJoin/ChatJoin.tsx
@@ -34,7 +34,7 @@ export default function ChatJoin({
 
     const [modalVisible, setModalVisible] = useState(false);
     const [modalData, setModalData] = useState<string>(courses[0]);
-    const [search, setSearch] = useState<string>('');
+    const [searchText, setSearchText] = useState<string>('');
 
     const openCourseModal = (id: string) => {
         setModalData(id);
@@ -46,6 +46,12 @@ export default function ChatJoin({
         // const title = `${courseId.replaceAll(/\s+/g, ' ').trim()} - ${
         // course.courseTitle
         // }`;
+        if (
+            course.courseTitle
+                .toLowerCase()
+                .replace(/\s/g, '')
+                .includes(searchText.toLowerCase().replace(/\s/g, '').trim())     
+        ){
             return (
             <View style={styles.courseCard}>
                 <Text style={styles.courseCardTitle}>{course.courseTitle}</Text>
@@ -67,6 +73,7 @@ export default function ChatJoin({
             </View>
             );
         }
+    }
     
     function generateCourseModal(courseId: string) {
         const course = coursemap[courseId];
@@ -106,6 +113,20 @@ return (
             <Text style={styles.headerText}>Join Chat</Text>
             <View style={{width: 40}}></View>
         </View>
+
+        <SearchBar 
+            containerStyle={{
+                backgroundColor: 'transparent',
+                borderBottomColor: 'transparent',
+                borderTopColor: 'transparent',
+                width: '97%',
+            }}
+            inputContainerStyle={{ backgroundColor: '#EEEEEE' }}
+            placeholder='Search by course name'
+            //@ts-ignore
+            onChangeText={(text) => setSearchText(text)}
+            value={searchText} 
+        />
 
         <ScrollView
             style={styles.courseList}
