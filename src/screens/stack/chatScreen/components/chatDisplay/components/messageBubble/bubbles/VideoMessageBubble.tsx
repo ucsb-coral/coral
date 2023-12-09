b1d705cf55c35cf78649995289fb4348d33e1a44
MarcuX009 <pk421933605@gmail.com>
2023-12-08 20:54:13 -0800

    :ambulance: both android and ios work

--- src/screens/stack/chatScreen/components/chatDisplay/components/messageBubble/bubbles/VideoMessageBubble.tsx
+++ src/screens/stack/chatScreen/components/chatDisplay/components/messageBubble/bubbles/VideoMessageBubble.tsx
@@ -1,22 +1,31 @@
-import React, {useState, useEffect, useRef, ReactNode} from 'react';
+import React, { useState, useEffect, useRef, ReactNode } from 'react';
 import MessageBubbleWrapper, {
   MessageBubbleWrapperProps,
 } from '../MessageBubbleWrapper';
-import {Text} from 'react-native';
-import {black} from '../../../../../../../../utilities/colors';
-import Video, {ResizeMode, VideoRef} from 'react-native-video';
+import { Text, Pressable, Modal, View } from 'react-native';
+import { black } from '../../../../../../../../utilities/colors';
+import Video, { ResizeMode, VideoRef } from 'react-native-video';
+// @ts-ignore
+import { VLCPlayer } from 'react-native-vlc-media-player';
 
 const MAX_WIDTH = 200; // if image width is greater than this, scale down
 
 export type Props = MessageBubbleWrapperProps & MediaMessageContent;
 // fileName not used yet
-export default function VideoMessageBubble({url, ...rest}: Props) {
+export default function VideoMessageBubble({ url, ...rest }: Props) {
   const [height, setHeight] = useState(MAX_WIDTH * (9 / 16));
   const videoRef = useRef<VideoRef>(null);
-
+  const [isVideoViewVisible, setVideoViewVisible] = useState(false);
+  const handleVideoPress = () => {
+    setVideoViewVisible(true);
+  };
   return (
-    <MessageBubbleWrapper {...rest}>
-      <Video
+    <>
+      <MessageBubbleWrapper {...rest}>
+        <Pressable onPress={handleVideoPress}>
+        <Text>Play Video</Text>
+        </Pressable>
+        {/* <Video
         source={{uri: url}}
         ref={videoRef}
         style={{width: MAX_WIDTH, height: MAX_WIDTH * (9 / 16)}}
@@ -24,6 +33,25 @@ export default function VideoMessageBubble({url, ...rest}: Props) {
         controls={true}
         paused={true}
       />
-    </MessageBubbleWrapper>
+      */}
+        <Modal
+          visible={isVideoViewVisible}
+          transparent={true}
+          onRequestClose={() => setVideoViewVisible(false)}
+          style={{ flex: 1 }}>
+          <Pressable onPress={() => setVideoViewVisible(false)} style={{ flex: 1 }}>
+            <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.9)', justifyContent: 'center', alignItems: 'center' }}>
+              <VLCPlayer
+                source={{ uri: url }}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="contain"
+                controls={true}
+                paused={true}
+              />
+            </View>
+          </Pressable>
+        </Modal>
+      </MessageBubbleWrapper>
+    </>
   );
 }
